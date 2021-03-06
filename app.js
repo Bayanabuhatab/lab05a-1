25a3113fe299390f057777072869bc71dfd94fe7
bayanabuhatab <bayan.m.a@hotmail.com>
2021-03-06 19:40:02 +0200

    proplem2

--- app.js
+++ app.js
@@ -10,18 +10,18 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
 
-    let answer = a+b;
+   // let answer = a+b;
     
-    let newSent = `The sum of ${a} and ${b} is ${answer}.`;
+   // let newSent = `The sum of ${a} and ${b} is ${answer}.`;
 
-    let Array = [answer, newSent];
-    return Array;
+   // let Array = [answer, newSent];
+   // return Array;
   }
 
 
  //Here is the test for sum(); uncomment it to run it
 
- testSum(4,7);
+ //testSum(4,7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -36,10 +36,18 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 
+    let product = a * b;
+    
+    let message = `The product of ${a} and ${b} is ${product}.`;
+    let result = [product, message];
+  
+    return result;
+  
+
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
